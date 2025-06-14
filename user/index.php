d917c065df164b7b382c82882119dbc3cf4e34dc
Raihan2103 <m.rehannofal@gmail.com>
2025-06-14 09:37:05 +0700

    update index

--- user/index.php
+++ user/index.php
@@ -32,7 +32,7 @@ $buku_terbaru = $database->koneksi
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
-            <a class="navbar-brand fw-bold">PERPUSTAKAAN</a>
+            <a class="navbar-brand fw-bold">PERPUSTAKAAN ONLINE</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
